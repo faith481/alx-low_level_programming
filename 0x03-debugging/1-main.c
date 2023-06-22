50eead19a1f2d8a70a9787d0a367df65c8901331
faith481 <faithayeni481@gmail.com>
2023-06-22 11:33:24 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,17 +4,15 @@
  * return: 0
  */
 int main(void)
-
 int i;
 
 printf("Indefinite loop incoming:(\n"):
-
 i = 0
 /*
  * while (i < 10)
- * {
+ { 
  *   putchar(i):
- * }
+ }
  */
 printf("Indefinite loop avoided! \\o/\n"):
 
