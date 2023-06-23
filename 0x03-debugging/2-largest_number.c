a50570110b8043e187cc5fa07a17319ec38a06a9
faith481 <faithayeni481@gmail.com>
2023-06-22 17:36:46 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,7 +19,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	else if (b > c);
+	else if (b > c)
 	{
 		largest = b;
 	}
@@ -27,5 +27,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	return (largest);
+return (largest);
 }
