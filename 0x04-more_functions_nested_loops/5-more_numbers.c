caf3d7564173ad33095521fd17c57f37684da2c4
faith481 <faithayeni481@gmail.com>
2023-06-26 05:12:35 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - print 10 times the numbers since 0 up to 14
- * Return: 10 tines of the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
  */
 
 void more_numbers(void)
