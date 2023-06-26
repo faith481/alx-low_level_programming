b893586b66f73cb4f277f75cfce7c13d7ca06d6a
faith481 <faithayeni481@gmail.com>
2023-06-26 10:54:51 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    print_most_numbers(1);
-    print_most_numbers(3);
-    print_most_numbers(5);
-    print_most_numbers(6);
-    print_most_numbers(7);
-    print_most_numbers(8);
-    print_most_numbers(9);
-    return (0);
+print_most_numbers(1);
+print_most_numbers(3);
+print_most_numbers(5);
+print_most_numbers(6);
+print_most_numbers(7);
+print_most_numbers(8);
+print_most_numbers(9);
+return (0);
 }
