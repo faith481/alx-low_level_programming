eaae10f4254990f513f8bc579d869ae0068af162
faith481 <faithayeni481@gmail.com>
2023-07-05 12:24:32 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,17 +1,21 @@
 #include "main.h"
-
 /**
- *  wildcmp - compare two strings and returns them
- *  @s1: string 1.
- *  @s2: string 2. it can contain a * as a special character
- *  Return: 1 if are identical, if not 0
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
 	if (*s2 != '\0' && *s2 == '*')
+	{
+	return (wildcmp(s1, s2 + 1));
+	}
+	return (*s2 == '\0');
+	}
+	if (*s2 == '*')
 	{
 	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
