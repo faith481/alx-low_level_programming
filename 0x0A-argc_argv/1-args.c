a004e190af48a8d5ad42cf8a3b454ba07d51fdf9
faith481 <faithayeni481@gmail.com>
2023-07-07 13:54:57 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
 /**
  *  main - prints the number of arguments passed into a program
  *  @argc: number of arguments
- *  @argv: number of arrays
+ *  @argv: array of arguments
  *  Return: Always 0 (success)
  */
 
