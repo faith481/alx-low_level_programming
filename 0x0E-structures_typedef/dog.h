480af90ceff1d235ced9ba4d89fab5b575c3ece4
faith481 <faithayeni481@gmail.com>
2023-07-18 03:36:04 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,20 +3,20 @@
 
 /**
  * struct dog - A dog's basic information
- * @name: First element
- * @age: Second element
- * @owner: Third element
- * Description: Longer description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
 
 struct dog
 
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
 /**
  * dog_t - typedef for struct dog
  */
