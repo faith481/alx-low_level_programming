f21f0a16336ce04cd1972713824d3dfd949384fa
faith481 <faithayeni481@gmail.com>
2023-07-18 09:49:19 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,15 +5,16 @@
 /**
  * print_dog - prints a struct dog
  * @d: struct dog to print
-*/
-
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	return;
+
 	if (d->name == NULL)
 	d->name = "(nil)";
 	if (d->owner == NULL)
-	d->owner == "(nil)";
+	d->owner = "(nil)";
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
