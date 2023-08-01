3410a0132efa7846a67e48e146afd1c802bb823c
faith481 <faithayeni481@gmail.com>
2023-07-31 23:59:30 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint2 - frees a listint_t list
- * @head: pointer to the listint_t list to be fixed
+ * @head: pointer to the listint_t list to be freed
  */
 
 void free_listint2(listint_t **head)
@@ -16,6 +16,7 @@ void free_listint2(listint_t **head)
 	temp = (*head)->next;
 	free(*head);
 	*head = temp;
+	}
 	head = NULL;
 	}
 }
