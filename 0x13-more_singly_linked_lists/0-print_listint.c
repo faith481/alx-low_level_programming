b031821bb0ec3484dd4bf3af82258c5323b5dc3f
faith481 <faithayeni481@gmail.com>
2023-07-31 23:43:58 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of listint_t list
+ * print_listint - prints all elements of a listint_t list
  * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
 
-size_t print_listint_t(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
