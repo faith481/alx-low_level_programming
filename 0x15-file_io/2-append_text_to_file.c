d53b2e288f4920e79775751340317079d169b486
faith481 <faithayeni481@gmail.com>
2023-09-04 06:22:13 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,9 +20,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len];)
 	len++;
 	}
-	w = write(o, text_content, len);
 	o = open(filename, O_WRONLY | O_APPEND);
-	if (w == -1 || o == -1)
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
 	return (-1);
 	close(0);
 	return (1);
