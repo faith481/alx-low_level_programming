0a556517c416fa9075f5edf74fb49499c70a4399
faith481 <faithayeni481@gmail.com>
2023-09-04 23:47:09 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -19,18 +19,25 @@ void close_elf(int elf);
 
 /**
  * check_elf - checks if a file is an ELF file
- * @ptr: magic 
+ * @e_ident: a pointer to an array containing the ELF magic numbers
+ * Description: if the file is not an ELF file exit code 98
  */
 
-void check_elf(unsigned char *ptr)
+void check_elf(unsigned char *e_ident)
 {
-	int e_ident = (int)ptr[0];
-	char E = ptr[1];
-	char L = ptr[2];
-	char F = ptr[3];
+	int index;
 
-	if (e_ident == 127 && E == 'E' && L == 'L' && F == 'F')
-	return;
+	for (index = 0; index < 4; index++)
+		{
+	if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
+		{
+		dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+		exit(98);
+		}
+	}
 }
 
 /**
