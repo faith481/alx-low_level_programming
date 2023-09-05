3cb5c7b91e0d78dad95918713e9c036bf151fff4
faith481 <faithayeni481@gmail.com>
2023-09-04 19:16:46 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -107,9 +107,17 @@ void print_data(unsigned char *e_ident)
 
 void print_version(unsigned char *e_ident)
 {
-	printf("Version: %d(current)\n", e_ident[EI_VERSION]);
+	printf("Version: %d", e_ident[EI_VERSION]);
+	switch (e_ident[EI_VERSION])
+	{
+	case EV_CURRENT:
+		printf("current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
+	}
 }
-
 /**
  * print_osabi - prints the OS/ABI of an ELF header
  * @e_ident: a pointer to an array containing the ELF version
