174757eff3ee89009273f83848b2bb68dfd2bfe9
faith481 <faithayeni481>
2023-09-28 04:00:42 -0700

    converts a binary number to an unsigned int

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -8,16 +8,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
+	int index;
 	unsigned int dec_val = 0;
 
 	if (!b)
-	return (0);
-	for (i = 0; b[i]; i++)
+		return (0);
+	for (index = 0; b[index]; index++)
 	{
-	if (b[i] < '0' || b[i] > '1')
+	if (b[index] < '0' || b[index] > '1')
 	return (0);
-	dec_val = 2 * dec_val + (b[i] - '0');
+	dec_val = 2 * dec_val + (b[index] - '0');
 	}
 	return (dec_val);
 }
