ca0a0001dda0ab98f083fb69eedd910880f10c64
faith481 <faithayeni481@gmail.com>
2023-10-02 05:36:14 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 		len++;
 	}
-	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 	if (fd == -1 || w == -1)
 		return (-1);
